9c64f407203644a257d1ff675888cdf916d212c4
Krish Ram <krish.ram@my.bdsc.school.nz>
2026-08-07 17:16:29 +1200

    Add the app title heading

--- code/EmergencyQ_Version1.py
+++ code/EmergencyQ_Version1.py
@@ -10,4 +10,9 @@ root.title("Emergency Q - Version 1")
 root.geometry("420x700")
 root.configure(bg=WHITE)
 
+# Title heading at the top of the window
+title = tk.Label(root, text="Emergency Q Prototype",
+                 bg=WHITE, fg=INK, font=(FONT, 16, "bold"))
+title.pack(pady=(18, 14))
+
 root.mainloop()
